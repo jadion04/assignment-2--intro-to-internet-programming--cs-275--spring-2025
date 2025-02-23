3561b9d5620555267665330ad2aca6d4f59bfe41
Roy Vanegas <roy@thecodeeducators.com>
2025-02-23 12:54:25 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// -----------------------------------------------------------------------------
+// This file includes deliberate formatting errors in order for you to verify
+// that ESLint and EditorConfig are working properly. If both tools are, indeed,
+// working correctly, then you’d see errors in your editor about indentation and
+// improper use of footmarks instead of back ticks. When you save this file,
+// your editor should strip all excess newlines and whitespace characters from
+// the file. If both of these events occur, then ESLint and EditorConfig are
+// working correctly.
+//
+// DON’T PROCEED UNTIL YOU’RE SURE ESLINT AND EDITORCONFIG ARE WORKING CORRECTLY
+// -----------------------------------------------------------------------------
+window.onload = () => {
+       alert('Test');                    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
